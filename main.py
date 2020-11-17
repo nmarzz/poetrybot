ce9bd821b0b031337e40738a4bf7f9bf4d51dc5c
nmarzz <ndmarshall97@gmail.com>
2020-11-17 11:34:11 -0800

    add WMT14

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ print(device)
 my_tok = spacy.load('en')
 def spacy_tok(x):
     return [tok.text for tok in my_tok.tokenizer(x)]
-TEXT = data.Field(lower=True, tokenize=spacy_tok)
+SRC = data.Field(lower=True, tokenize=spacy_tok)
+TRG = data.Field(lower=True, tokenize=spacy_tok)
 
-train, valid, test = WMT14.splits(('.en','.de'),TEXT) # loading custom datasets requires passing in the field, but nothing else.
-TEXT.build_vocab(train, vectors="glove.6B.200d")
+train, valid, test = WMT14.splits(exts=('.de', '.en'),
+                                           train='train.tok.clean.bpe.32000',
+                                           validation='newstest2013.tok.bpe.32000',
+                                           test='newstest2014.tok.bpe.32000',
+                                           fields=(SRC, TRG))
 
-train_iter, valid_iter, test_iter = data.BPTTIterator.splits(
-    (train, valid, test),
-    batch_size=32,
-    bptt_len=30, # this is where we specify the sequence length
-    device=device,
-    repeat=False)
+# TEXT.build_vocab(train, vectors="glove.6B.200d")
+#
+# train_iter, valid_iter, test_iter = data.BPTTIterator.splits(
+#     (train, valid, test),
+#     batch_size=32,
+#     bptt_len=30, # this is where we specify the sequence length
+#     device=device,
+#     repeat=False)
