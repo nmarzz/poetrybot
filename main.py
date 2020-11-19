44de3712a9d2f65cb45443135d2176de882f3776
nmarzz <ndmarshall97@gmail.com>
2020-11-18 16:05:44 -0800

    Remove WMT14-add customs data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import torch.utils.data
 import torchtext
 from torchtext import data
 import spacy
@@ -10,25 +11,18 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
 
-print(device)
-
 my_tok = spacy.load('en')
 def spacy_tok(x):
     return [tok.text for tok in my_tok.tokenizer(x)]
-SRC = data.Field(lower=True, tokenize=spacy_tok)
-TRG = data.Field(lower=True, tokenize=spacy_tok)
+TEXT = data.Field(tokenize=spacy_tok)
 
-train, valid, test = WMT14.splits(exts=('.de', '.en'),
-                                           train='train.tok.clean.bpe.32000',
-                                           validation='newstest2013.tok.bpe.32000',
-                                           test='newstest2014.tok.bpe.32000',
-                                           fields=(SRC, TRG))
 
-# TEXT.build_vocab(train, vectors="glove.6B.200d")
-#
-# train_iter, valid_iter, test_iter = data.BPTTIterator.splits(
-#     (train, valid, test),
-#     batch_size=32,
-#     bptt_len=30, # this is where we specify the sequence length
-#     device=device,
-#     repeat=False)
+Mydata = torchtext.datasets.LanguageModelingDataset(path = 'scpoems.txt',text_field=TEXT,newline_eos =False )
+TEXT.build_vocab(Mydata)
+
+loader = torchtext.data.BPTTIterator(dataset = Mydata,batch_size = 32,bptt_len = 30,shuffle = True,device = device)
+
+batch = (next(iter(loader)))
+
+print(getattr(batch,"text"))
+print(getattr(batch,"target"))
