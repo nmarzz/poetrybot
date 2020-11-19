e0c7605f109a029ac9993be50e89121eaf86f12b
nmarzz <ndmarshall97@gmail.com>
2020-11-18 17:23:22 -0800

    add test/train

--- main.py
+++ main.py
@@ -3,26 +3,46 @@ import torch.utils.data
 import torchtext
 from torchtext import data
 import spacy
-from torchtext.datasets import WikiText2,WMT14
+from torchtext.datasets import WikiText2
 from spacy.symbols import ORTH
 
+# Device: set up kwargs for future
 if torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
 
+# Define tokenizer and field (field will be deprecated soon)
 my_tok = spacy.load('en')
 def spacy_tok(x):
     return [tok.text for tok in my_tok.tokenizer(x)]
 TEXT = data.Field(tokenize=spacy_tok)
 
+# Define data and iterator
+train = torchtext.datasets.LanguageModelingDataset(path = 'poems.txt',text_field=TEXT,newline_eos =False )
+test = torchtext.datasets.LanguageModelingDataset(path = 'poems_test.txt',text_field=TEXT,newline_eos =False )
 
-Mydata = torchtext.datasets.LanguageModelingDataset(path = 'scpoems.txt',text_field=TEXT,newline_eos =False )
-TEXT.build_vocab(Mydata)
+TEXT.build_vocab(train, vectors="glove.6B.200d")
 
-loader = torchtext.data.BPTTIterator(dataset = Mydata,batch_size = 32,bptt_len = 30,shuffle = True,device = device)
+train_iter, test_iter = data.BPTTIterator.splits((train, test),
+    batch_size=32,
+    bptt_len=30,
+    device=device,
+    repeat=False)
 
-batch = (next(iter(loader)))
 
-print(getattr(batch,"text"))
-print(getattr(batch,"target"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
